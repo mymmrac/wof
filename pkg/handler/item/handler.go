b6544e0307d47d30d14a39ce83181ea1b7c34b8b
Artem Yadelskyi <mymmrac@gmail.com>
2026-06-28 21:25:28 +0300

    Fix fallback image

--- pkg/handler/item/handler.go
+++ pkg/handler/item/handler.go
@@ -306,6 +306,12 @@ func (h *handler) updateItemOrderHandler(fCtx fiber.Ctx) error {
 	return fCtx.JSON(fiber.Map{"ok": true})
 }
 
+const fallbackImage = `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#3f4f56">
+    <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zm-5.04-6.71l-2.75 3.54-1.96-2.36L6.5 17h11l-3.54-4.71z"/>
+</svg>
+`
+
 func (h *handler) getImageHandler(fCtx fiber.Ctx) error {
 	var request struct {
 		WheelID id.ID `uri:"wheelID" validate:"required"`
@@ -332,7 +338,8 @@ func (h *handler) getImageHandler(fCtx fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusInternalServerError)
 	}
 	if !found {
-		return fiber.NewError(fiber.StatusNotFound)
+		fCtx.Set(fiber.HeaderContentType, "image/svg+xml")
+		return fCtx.Send([]byte(fallbackImage))
 	}
 
 	contentType := http.DetectContentType(imageData)
